c2228fa0c69714f21f08c807d83ef689b6919bde
michael beamer <beamermichael@gmail.com>
2021-01-17 21:56:06 +0100

    fixed the escaping of quotes and other special chars

--- quiz.js
+++ quiz.js
@@ -18,18 +18,18 @@ const maxQuestions = 10;
 let questions = [];
 
 const url =
-  "https://opentdb.com/api.php?amount=50&difficulty=easy&type=multiple";
+  "https://opentdb.com/api.php?amount=20&difficulty=easy&type=multiple";
 fetch(url)
   .then((res) => {
     return res.json();
   })
   .then((loadedQuestions) => {
-    console.log(loadedQuestions.results);
     questions = loadedQuestions.results.map((loadedQuestion) => {
       const formattedQuestion = {
-        question: loadedQuestion.question,
+        question: _.unescape(loadedQuestion.question),
       };
 
+      console.log(loadedQuestion.question);
       const answersChoices = [...loadedQuestion.incorrect_answers];
       formattedQuestion.correctAnswer = Math.floor(Math.random() * 3) + 1;
       answersChoices.splice(
@@ -41,30 +41,10 @@ fetch(url)
       answersChoices.forEach((choice, index) => {
         formattedQuestion["answer" + (index + 1)] = choice;
       });
-      console.log(formattedQuestion);
       return formattedQuestion;
     });
     startGame();
   });
-
-// let questions = [
-//   {
-//     question: "Who is richer?",
-//     answer1: "Kanye West",
-//     answer2: "Big but Kim",
-//     answer3: "P.Diddy",
-//     answer4: "Golum",
-//     correctAnswer: 4,
-//   },
-//   {
-//     question: "Who is nicer?",
-//     answer1: "Winnie Pooh",
-//     answer2: "Bob the Builder",
-//     answer3: "Morty",
-//     answer4: "Wall-E",
-//     correctAnswer: 3,
-//   },
-// ];
 
 function startGame() {
   questionCounter = 0;
@@ -88,13 +68,13 @@ function getNewQuestion() {
   // Get a randomNumber, so that a random question(object) can get picked from the array
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
-  questionEl.textContent = currentQuestion.question;
+  questionEl.innerHTML = currentQuestion.question;
 
   // Iteration over the array of answer divs - to change the content text of each of them
   // with dataset its easier option b would be a counter to increment the number
   answerEl.forEach((answer) => {
     const dataNumber = answer.dataset.number;
-    answer.textContent = currentQuestion[`answer${dataNumber}`];
+    answer.innerHTML = currentQuestion[`answer${dataNumber}`];
   });
   // Slices the used question from the array
   availableQuestions.splice(questionIndex, 1);
